0070c2683547dc93c5ac66eb0f1a90a936e0828a
Scott Peat <scottpeatweb@gmail.com>
2022-11-02 07:45:08 +0000

    Continue the work on NFTMarketplace.sol

--- hardhat/contracts/NFTMarketplace.sol
+++ hardhat/contracts/NFTMarketplace.sol
@@ -4,5 +4,14 @@ pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
 contract NFTMarketplace {
+
+    struct Listing {
+        uint256 price;
+        adddress seller;
+    }
+
+    mapping(address => mapping(uint256 => Listing)) public listings;
+
+    
     
 }
