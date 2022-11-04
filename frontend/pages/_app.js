7ed0b1a243ff4ce8294e0fe2ae591d4a64a2de8d
Scott Peat <scottpeatweb@gmail.com>
2022-11-04 16:28:03 +0000

    Added RainbowKit and now starting work on frontend

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,7 +1,6 @@
-import '../styles/globals.css'
+import '@rainbow-me/rainbowkit/styles.css';
+import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
-
-export default MyApp
+import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import { configureChains, createClient, WagmiConfig } from 'wagmi';
+import { jsonRpcProvider } from 'wagmi/providers/jsonRpc';
