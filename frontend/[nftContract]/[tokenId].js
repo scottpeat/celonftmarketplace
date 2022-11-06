142226bef8cb03c4be7bbec0b8c5144784e8f8eb
Scott Peat <scottpeatweb@gmail.com>
2022-11-06 18:47:23 +0000

    Starting work on the NFT Contract listing page

--- frontend/[nftContract]/[tokenId].js
+++ frontend/[nftContract]/[tokenId].js
@@ -1,6 +1,45 @@
+import { Contract } from 'ethers';
+import { formatEther, parseEther } from 'ethers/lib/utils';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import { createClient } from 'urql';
+import { useContract, useSigner, erc721ABI } from 'wagmi';
+import MarketplaceABI from '../../abis/NFTMarketplace.json';
+import Navbar from '../../components/Navbar';
+import { MARKETPLACE_ADDRESS, SUBGRAPH_URL } from '../../constants';
 import styled from 'styled-components';
 
-// CSS
+export default function NFTDetails() {
+  // Extract NFT contract address and Token ID from URL
+  const router = useRouter();
+  const nftAddress = router.query.nftContract;
+  const tokenId = router.query.tokenId;
+
+  // State variables to contain NFT and listing information
+  const [listing, setListing] = useState();
+  const [name, setName] = useState('');
+  const [imageURI, setImageURI] = useState('');
+  const [isOwner, setIsOwner] = useState(false);
+  const [isActive, setIsActive] = useState(false);
+
+  // State variable to contain new price if updating listing
+  const [newPrice, setNewPrice] = useState('');
+
+  // State variables to contain various loading states
+  const [loading, setLoading] = useState(true);
+  const [updating, setUpdating] = useState(true);
+  const [canceling, setCanceling] = useState(false);
+  const [buyer, setBuying] = useState(false);
+
+  // Fetch signer from wagmi
+  const { data: signer } = useSigner();
+
+  const MarketplaceContract = useContract({
+    addressOrName: MARKETPLACE_ADDRESS,
+    contractInterface: MarketplaceABI,
+    signerOrProvider: signer,
+  });
+}
 
 const Container = styled.div`
   display: flex;
