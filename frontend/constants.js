d9a2e3187616a232154164b66554f460ed07c7be
Scott Peat <scottpeatweb@gmail.com>
2022-11-08 08:20:21 +0000

    Replaced solidity files

--- frontend/constants.js
+++ frontend/constants.js
@@ -1,3 +1,3 @@
 export const SUBGRAPH_URL =
   'https://api.thegraph.com/subgraphs/name/scottpeat/celomarketplace';
-export const MARKETPLACE_ADDRESS = '0xEF43C1A81099937F83220546056a5fDb046eAe3d';
+export const MARKETPLACE_ADDRESS = '0xE9cFE54BcD4d1FaaADa4215E8b89e82BB0e9530b';
